e85dcf80dbd2e5b222df4f350423e2f4259af899
Dino <72475325+dinoclier72@users.noreply.github.com>
2023-01-23 23:58:42 +0100

    fichier template database connect

---
+++ components/database_server.php
@@ -0,0 +1,9 @@
+<?php
+    //fichier à completer
+    $Server = ""; //database server
+    $User =  ""; //user
+    $Pwd = ""; //password
+    $db = ""; //database name
+
+    $database = new mysqli($Server,$User,$Pwd,$db);
+?>
